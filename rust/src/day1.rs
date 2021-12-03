57b34fa7c74d368108a5e923b5dce6a78d26e673
Alexis Trudeau <atrudeau@hopper.com>
2021-12-03 17:58:20 -0500

    Rust: day1 - part 2

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -2,12 +2,39 @@ use std::fs::File;
 use std::io::{self, BufRead, Lines, BufReader};
 use std::path::Path;
 
-pub fn day1() -> u32 {
-   read_lines("../input/day1.txt")
-       .filter_map(|l| l.ok())
-       .map(|s| s.parse::<u32>().unwrap())
-       .fold((u32::MAX, 0), | (i0, n), i1 | (i1, n + u32::from(i1 > i0)))
-       .1
+pub fn day1_part1() -> u32 {
+    count_increases(&depths().collect::<Vec<u32>>())
+}
+
+pub fn day1_part2() -> u32 {
+    let rolling_sums: Vec<u32> = depths()
+        .collect::<Vec<u32>>()
+        .windows(3)
+        .filter_map(|w| match w {
+            [a, b, c] => Some(a + b + c),
+            _ => None
+        }).collect();
+
+    count_increases(&rolling_sums)
+}
+
+fn count_increases(depths: &Vec<u32>) -> u32 {
+    depths
+        .windows(2)
+        .filter_map(|w| match w {
+            [a, b] => Some((a, b)),
+            _ => None
+        })
+        .fold(0, |n, (d0, d1)| n + u32::from(d1 > d0))
+}
+
+/** @todo Need to understand what a Box is. (Seems to describe a heap allocated element). */
+fn depths() -> Box<dyn Iterator<Item=u32>> {
+    Box::new(
+        read_lines("../input/day1.txt")
+            .filter_map(|l| l.ok())
+            .map(|s| s.parse::<u32>().unwrap()).into_iter()
+    )
 }
 
 fn read_lines<P>(filename: P) -> Lines<BufReader<File>> where P: AsRef<Path> {
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,6 @@
 mod day1;
 
 fn main() {
-    println!("day1 answer = {}", day1::day1());
+    println!("day1-1 answer = {}", day1::day1_part1());
+    println!("day1-2 answer = {}", day1::day1_part2());
 }
