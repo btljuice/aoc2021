8a6176ed646910ff2156a33b7916db14a2abe4b1
plaflamme <philippe@hopper.com>
2021-12-03 21:20:56 -0500

    tidy: introduce itertools and use tuple_windows
    the .windows function is only available on slices, but it also doesn't allocate.
    
    Itertools has extensions for Iterators including one called tuple_windows which will produce sliding windows of up to 4 elements, but it needs to clone the elements.

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -2,8 +2,10 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+use itertools::Itertools;
+
 pub fn day1_part1() -> u32 {
-    count_increases(&depths().collect::<Vec<u32>>())
+    count_increases(depths())
 }
 
 pub fn day1_part2() -> u32 {
@@ -12,19 +14,16 @@ pub fn day1_part2() -> u32 {
         .windows(3)
         .filter_map(|w| match w {
             [a, b, c] => Some(a + b + c),
-            _ => None
-        }).collect();
-
-    count_increases(&rolling_sums)
-}
-
-fn count_increases(depths: &Vec<u32>) -> u32 {
-    depths
-        .windows(2)
-        .filter_map(|w| match w {
-            [a, b] => Some((a, b)),
             _ => None,
         })
+        .collect();
+
+    count_increases(rolling_sums.into_iter())
+}
+
+fn count_increases(depths: impl Iterator<Item = u32>) -> u32 {
+    depths
+        .tuple_windows()
         .fold(0, |n, (d0, d1)| n + u32::from(d1 > d0))
 }
 
