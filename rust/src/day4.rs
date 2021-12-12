eb0a41c39df7d97380fc8c22fb26a1aab9934562
Alexis Trudeau <atrudeau@hopper.com>
2021-12-11 20:16:06 -0500

    Rust: day 4 - Complete parsing tests

--- rust/src/day4.rs
+++ rust/src/day4.rs
@@ -1,6 +1,3 @@
-use std::fs::read;
-use std::convert::TryInto;
-use itertools::Itertools;
 use super::common;
 
 pub fn part1() -> u32 {
@@ -12,8 +9,10 @@ pub fn part2() -> u32 {
 }
 
 pub(self) mod bingo {
+    use super::common;
     use itertools::Itertools;
-    use std::convert::{TryFrom, TryInto};
+    use std::convert::TryFrom;
+    use std::convert::TryInto;
 
     pub const CARD_SIZE: usize = 5;
     pub type Number = u8;
@@ -39,6 +38,7 @@ pub(self) mod bingo {
     // ANSME: How to assess a crate's package adoption + maturity, apart looking at github repo?
     #[derive(Debug, PartialEq)]
     pub struct Card { pub rows: [CardRow; CARD_SIZE] }
+
     impl TryFrom< Vec<CardRow> > for Card {
         type Error = ParseError;
 
@@ -80,48 +80,81 @@ pub(self) mod bingo {
         }).try_collect::<_, Vec<_>, _>()?
           .try_into()
     }
+
+    pub fn parse(filename: &str) -> Result<(Draw, Vec<Card>), ParseError> {
+        fn chunk_to_card(chunk: impl Iterator<Item=String>) -> Result<Card, ParseError> {
+            let lines: [String; CARD_SIZE] = chunk
+                .skip(1)
+                .collect_vec()
+                .try_into().map_err(|_| ParseError::CardInvalidNbRows(0))?; // TODO
+            // Well, well, we're learning about lifetimes here. Array.map converts the array.
+            let slices: [&str; CARD_SIZE] = [ lines[0].as_str(), lines[1].as_str(), lines[2].as_str(), lines[3].as_str(), lines[4].as_str() ];
+            parse_card(&slices)
+        }
+
+        let mut lines = common::read_lines(filename);
+        let draw: Draw = parse_draw(lines.next().as_ref().map(|s| s.as_str()))?;
+        let cards: Vec<Card> = lines
+            .chunks(6)
+            .into_iter()
+            .map(chunk_to_card)
+            .try_collect()?;
+
+        Ok( (draw, cards) )
+    }
 }
 
 #[cfg(test)]
 pub(self) mod tests {
     use super::bingo::*;
-    use super::common;
 
-    const CARD: &'static [&'static str; CARD_SIZE] = &[
-        " 1  2  3  4  5",
-        " 6  7  8  9 10",
-        "11 12 13 14 15",
-        "16 17 18 19 20",
-        "21 22 23 24 25"
+    const CARD1_STR: &'static [&'static str; CARD_SIZE] = &[
+        "22 13 17 11  0",
+        "8  2 23  4 24",
+        "21  9 14 16  7",
+        "6 10  3 18  5",
+        "1 12 20 15 19"
     ];
+
+    const CARD1: Card = Card { rows: [
+        CardRow { row: [ 22, 13, 17, 11,  0 ] },
+        CardRow { row: [  8,  2, 23,  4, 24 ] },
+        CardRow { row: [ 21,  9, 14, 16,  7 ] },
+        CardRow { row: [  6, 10,  3, 18,  5 ] },
+        CardRow { row: [  1, 12, 20, 15, 19 ] }
+    ] };
+
+    const CARD2: Card = Card { rows: [
+        CardRow { row: [  3, 15,  0,  2, 22 ] },
+        CardRow { row: [  9, 18, 13, 17,  5 ] },
+        CardRow { row: [ 19,  8,  7, 25, 23 ] },
+        CardRow { row: [ 20, 11, 10, 24,  4 ] },
+        CardRow { row: [ 14, 21, 16, 12,  6 ] }
+    ] };
+
+    const CARD3: Card = Card { rows: [
+        CardRow { row: [ 14, 21, 17, 24,  4 ] },
+        CardRow { row: [ 10, 16, 15,  9, 19 ] },
+        CardRow { row: [ 18,  8, 23, 26, 20 ] },
+        CardRow { row: [ 22, 11, 13,  6,  5 ] },
+        CardRow { row: [  2,  0, 12,  3,  7 ] }
+    ] };
+
+    fn sample() -> Result<(Draw, Vec<Card>), ParseError> { parse("../input/day4_sample.txt") }
 
     #[test]
     fn test_parse_card() {
-        let actual = parse_card(CARD).unwrap();
-        let expected: Card = Card { rows: [
-            CardRow { row: [ 1,  2,  3,  4,  5] },
-            CardRow { row: [ 6,  7,  8,  9, 10] },
-            CardRow { row: [11, 12, 13, 14, 15] },
-            CardRow { row: [16, 17, 18, 19, 20] },
-            CardRow { row: [21, 22, 23, 24, 25] }
-        ] };
-        assert_eq!(actual, expected)
-    }
-
-    fn sample() -> Result<(Draw, Vec<Card>), ParseError> {
-        let mut lines = common::read_lines("../input/day4_sample.txt");
-        let draw: Draw = parse_draw(lines.next().as_ref().map(|s| s.as_str()))?;
-
-        let cards: Vec<Card> = Vec::new();
-
-        Ok( (draw, cards) )
+        let actual = parse_card(CARD1_STR).unwrap();
+        assert_eq!(actual, CARD1)
     }
 
     #[test]
     fn test1() {
+        let (draw, cards) = sample().unwrap();
         assert_eq!(
-            sample().unwrap().0,
+            draw,
             vec![7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
-        )
+        );
+        assert_eq!(cards, vec![CARD1, CARD2, CARD3]);
     }
 }
