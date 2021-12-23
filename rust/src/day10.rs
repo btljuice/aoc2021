c3145b94c8e644771e3238c9a3e68322378ebb6e
Alexis Trudeau <atrudeau@hopper.com>
2021-12-22 22:46:12 -0500

    Rust day 10 part 2

--- rust/src/day10.rs
+++ rust/src/day10.rs
@@ -1,37 +1,46 @@
 use std::borrow::Borrow;
 
-#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+use itertools::Itertools;
+use rdxsort::RdxSort;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
 enum ParseResult {
   Valid,
-  Incomplete,
+  Incomplete(String), // Missing delimiters
   Corrupted(char),
 }
 
-fn is_open_delimiter(c: char) -> bool { "([{<".contains(c) }
-
-fn are_matching_pair(open: char, close: char) -> bool {
-  match (open, close) {
-    ('(', ')') | ('[', ']') | ('<', '>') | ('{', '}') => true,
-    _ => false,
+fn match_closing_delimiter(open: char) -> Option<char> {
+  match open {
+    '(' => Some(')'),
+    '[' => Some(']'),
+    '{' => Some('}'),
+    '<' => Some('>'),
+    _ => None
   }
 }
+
 
 fn parse_line<S: AsRef<str>>(line: S) -> ParseResult {
   let mut stack: Vec<char> = Vec::new();
   for c in line.as_ref().chars() {
-    if is_open_delimiter(c) {
-      stack.push(c)
-    } else {
+    if let Some(close) = match_closing_delimiter(c) { stack.push(close); }
+    else {
       match stack.pop() {
         None => return ParseResult::Corrupted(c),
-        Some(o) =>  if !are_matching_pair(o, c) { return ParseResult::Corrupted(c) }
+        Some(close_expected) => if c != close_expected { return ParseResult::Corrupted(c) }
       }
     }
   }
-  if stack.is_empty() { ParseResult::Valid } else { ParseResult::Incomplete }
+
+  if stack.is_empty() { 
+    ParseResult::Valid 
+  } else {
+    ParseResult::Incomplete(stack.into_iter().rev().collect())
+  }
 }
 
-fn score<R: Borrow<ParseResult>>(results: impl Iterator<Item=R>) -> u64 {
+fn corrupted_score<R: Borrow<ParseResult>>(results: impl Iterator<Item=R>) -> u64 {
   results.map(|r| match r.borrow() {
     ParseResult::Corrupted(')') => 3,
     ParseResult::Corrupted(']') => 57,
@@ -39,6 +48,19 @@ fn score<R: Borrow<ParseResult>>(results: impl Iterator<Item=R>) -> u64 {
     ParseResult::Corrupted('>') => 25137,
     _ => 0
   }).sum()
+}
+
+fn incomplete_score<R: Borrow<ParseResult>>(results: impl Iterator<Item=R>) -> u64 {
+  fn pt(c: char) -> u64 { match c { ')' => 1, ']' => 2, '}' => 3, '>' => 4, _ => panic!("Unexpected character") } }
+
+  let mut scores = results.filter_map(|r| match r.borrow() {
+    ParseResult::Incomplete(completion) => Some(completion.chars().fold(0u64,|sum, c| sum*5 + pt(c))),
+    _ => None
+  }).collect_vec();
+
+  scores.rdxsort();
+
+  scores[scores.len()/2]
 }
 
 
@@ -86,18 +108,33 @@ use super::*;
   fn test_part1_sample() {
     let results = SAMPLE.split('\n').map(parse_line).collect_vec();
     let expected = vec![
-      Incomplete,     Incomplete, Corrupted('}'), Incomplete,     Corrupted(')'),
-      Corrupted(']'), Incomplete, Corrupted(')'), Corrupted('>'), Incomplete
+      Incomplete("}}]])})]".to_string()),
+      Incomplete(")}>]})".to_string()),
+      Corrupted('}'),
+      Incomplete("}}>}>))))".to_string()),
+      Corrupted(')'),
+      Corrupted(']'),
+      Incomplete("]]}}]}]}>".to_string()),
+      Corrupted(')'),
+      Corrupted('>'),
+      Incomplete("])}>".to_string()),
     ];
     assert_eq!(results, expected);
-    assert_eq!(score(results.iter()), 26397);
+    assert_eq!(corrupted_score(results.iter()), 26397);
+    assert_eq!(incomplete_score(results.iter()), 288957);
   }
 
   #[test]
   fn part1() {
-    let results = common::parse::read_lines("../input/day10.txt").map(parse_line);
-    let total_score =  score(results);
-    println!("day9 part1 answer = {}", total_score);
+    let results = common::parse::read_lines("../input/day10.txt").map(parse_line).collect_vec();
+    let corrupted =  corrupted_score(results.iter());
+    let incomplete = incomplete_score(results.iter());
+    println!("day10 part1 answer = {}", corrupted);
+    println!("day10 part1 answer = {}", incomplete);
+
+    assert_eq!(corrupted, 358737);
+    assert_eq!(incomplete, 4329504793);
+
   }
 
 
